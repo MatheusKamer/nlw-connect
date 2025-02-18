19cfc1b0c8f04686b1024b4df30bee3fb019c569
Matheus Kamer <matheus_kamer@hotmail.com>
2025-02-17 21:18:31 -0400

    feat: update Button component to accept children and enforce label prop

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,13 @@
 interface ButtonProps {
-  label?: string;
+  children?: React.ReactNode;
+  label: string;
 }
 
-export function Button({ label }: ButtonProps) {
+export function Button({ label, children }: ButtonProps) {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      {label || 'Enviar'}
+    <button className="text-blue hover:bg-blue flex h-12 w-full cursor-pointer items-center justify-between rounded-xl bg-gray-500 px-5 font-semibold transition-colors duration-300 hover:text-gray-900">
+      {label}
+      {children}
     </button>
   );
 }
